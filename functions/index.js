df9d11934e5c2e2e150ac4e013d5367aada09c80
Marcin Pilarczyk <marcinpilarczyk@me.com>
2019-11-26 20:24:23 +0100

    Correction in get all screams

--- functions/index.js
+++ functions/index.js
@@ -11,21 +11,24 @@ app.get("/screams", (req, res) => {
   admin
     .firestore()
     .collection("screams")
-    .orderBy("createdAt", "desc")
     .get()
     .then(data => {
       let screams = [];
+      if (data.length == 0) {
+        return res.status(200).json({ message: "There is no screams" });
+      }
       data.forEach(doc => {
         screams.push({
           screamId: doc.id,
-          body: doc.data().body,
           userHandle: doc.data().userHandle,
           createdAt: doc.data().createdAt
         });
-        return res.json(screams);
       });
+      return res.json(screams);
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
 });
 
 app.post("/scream", (req, res) => {
