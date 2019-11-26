3ff2b8321546d38fd304a75e4ab8516aa314c264
Marcin Pilarczyk <marcinpilarczyk@me.com>
2019-11-26 19:42:23 +0100

    Adding express to serve all requests from one endpoint.

--- functions/index.js
+++ functions/index.js
@@ -1,58 +1,53 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const express = require("express");
+var cors = require("cors");
 
 admin.initializeApp();
+const app = express();
+app.use(cors());
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
-exports.getScreams = functions
-  .region("europe-west2")
-  .https.onRequest((request, response) => {
-    if (req.method !== "GET") {
-      return res.status(400).json({ error: "Method not allowed" });
-    }
-
-    admin
-      .firestore()
-      .collection("screams")
-      .get()
-      .then(data => {
-        let screams = [];
-        data.forEach(doc => {
-          screams.push(doc.data());
+app.get("/screams", (req, res) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then(data => {
+      let screams = [];
+      data.forEach(doc => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
         });
-        return response.json(screams);
-      })
-      .catch(err => console.log(err));
-  });
-
-exports.createScream = functions
-  .region("europe-west2")
-  .https.onRequest((req, res) => {
-    if (req.method !== "POST") {
-      return res.status(400).json({ error: "Method not allowed" });
-    }
-
-    const newScream = {
-      body: req.body.body,
-      userHandle: req.body.userHandle,
-      createdAt: admin.firestore.Timestamp.fromDate(new Date())
-    };
-
-    admin
-      .firestore()
-      .collection("screams")
-      .add(newScream)
-      .then(doc => {
-        res.json({ message: `Document ${doc.id} created successfully` });
-      })
-      .catch(err => {
-        res.status(500).json({
-          error: "Something went wrong",
-          errorLog: err
-        });
-        console.error(err);
+        return res.json(screams);
       });
-  });
+    })
+    .catch(err => console.log(err));
+});
+
+app.post("/scream", (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString()
+  };
+
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then(doc => {
+      res.json({ message: `Document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "Something went wrong",
+        errorLog: err
+      });
+    });
+});
+
+exports.api = functions.region("europe-west2").https.onRequest(app);
