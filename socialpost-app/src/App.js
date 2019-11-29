d9f92efa2bd1d1eddacb3f4ec90ae381719f1b21
Marcin Pilarczyk <marcinpilarczyk@me.com>
2019-11-29 15:17:54 +0100

    Adding material ui and navigation

--- socialpost-app/src/App.js
+++ socialpost-app/src/App.js
@@ -4,21 +4,25 @@ import "./App.css";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
