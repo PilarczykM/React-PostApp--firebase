44131ed7d527da35701536c54d355e4198ef4986
Marcin Pilarczyk <marcinpilarczyk@me.com>
2019-11-29 14:56:09 +0100

    Creating basic router with react-router-dom

--- socialpost-app/src/App.js
+++ socialpost-app/src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
