ca15c5a1fc01046ec10fc72fe7357d6ba7980504
Marcin Pilarczyk <marcinpilarczyk@me.com>
2019-11-29 11:30:16 +0100

    Adding notification

--- functions/index.js
+++ functions/index.js
@@ -13,12 +13,16 @@ const {
 const {
   addUserDetails,
   getAuthenticatedUser,
+  getUserDetails,
   login,
+  markNotificationsRead,
   signUp,
   uploadImage
 } = require("./handlers/users");
 
 const FBAuth = require("./util/FBAuth");
+
+const { db } = require("./util/admin");
 
 // * === Scream routes === *
 // =========================
@@ -31,11 +35,89 @@ app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
 app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
 
 // * === User routes === *
-// =========================
+// =======================
 app.post("/signup", signUp);
 app.post("/login", login);
 app.post("/user/image", FBAuth, uploadImage);
 app.post("/user", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthenticatedUser);
+app.get("/user/:handle", getUserDetails);
+
+// * === Notification routes === *
+// =======================
+app.post("/notifications", FBAuth, markNotificationsRead);
 
 exports.api = functions.region("europe-west2").https.onRequest(app);
+
+// * === On like notification === *
+// ================================
+exports.createNotificationOnLike = functions
+  .region("europe-west2")
+  .firestore.document("/likes/{id}")
+  .onCreate(snapshot => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            screamId: doc.id
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.err(err);
+        return;
+      });
+  });
+
+// * === On comment notification === *
+// ===================================
+exports.createNotificationOnComment = functions
+  .region("europe-west2")
+  .firestore.document("/comments/{id}")
+  .onCreate(snapshot => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            screamId: doc.id
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.err(err);
+        return;
+      });
+  });
+
+// * === Delete notification on unlike === *
+// =========================================
+exports.deleteNotificationOnUnLike = functions
+  .region("europe-west2")
+  .firestore.document("likes/{id}")
+  .onDelete(snapshot => {
+    return db
+      .doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
